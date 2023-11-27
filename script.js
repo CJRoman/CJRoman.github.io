065ae37e8eeccdfa8951f4d70e078f6e227469bb
Roman Yarygin <ryarygin@livegenic.com>
2023-11-27 17:20:44 +1000

    Voice recognition

--- script.js
+++ script.js
@@ -5,20 +5,47 @@ const STATUSES = {
 };
 
 LOCAL_STORAGE_KEY = "tasks";
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 class Application {
 
   constructor() {
     this.tasks = [];
-    this.createTaskBtn = document.getElementById("create-task-button");
+    this.editMode = false;
+    this.editedTask = null;
+
     this.todoTasksList = document.getElementById("todo-tasks-list");
     this.inProgressTasksList = document.getElementById("in-progress-tasks-list");
     this.doneTasksList = document.getElementById("done-tasks-list");
     this.toggleThemeBtn = document.getElementById("toggle-theme");
+
+    this.createTaskBtn = document.getElementById("create-task-button");
+    this.taskInput = document.getElementById("create-task-input");
+    this.voiceInputBtn = document.getElementById("voice-input-button");
+
+    if (window.SpeechRecognition) {
+      this.speechRecognition = new SpeechRecognition();
+      this.speechRecognition.interimResults = true;
+      this.speechRecognition.lang = 'ru-RU';
+      this.speechRecognition.addEventListener('result', this.handleSpeechRecognition.bind(this));
+      this.speechRecognition.onerror = (e) => {
+        console.log(e.error);
+
+        if (e.error == 'no-speech') {
+          this.voiceInputBtn.classList.remove('active');
+          this.taskInput.placeholder = "Введите название задачи";
+        }
+      }
+    }
   }
 
   run() {
-    this.createTaskBtn.addEventListener("click", () => this.addTask());
+    this.createTaskBtn.addEventListener("click", () => this.updateTask());
+    this.taskInput.addEventListener("keypress", (e) => {
+      if (e.key === 'Enter') {
+        this.updateTask();
+      }
+    });
     this.toggleThemeBtn.addEventListener("click", () => {
       if (document.body.classList.contains("night")) {
         document.body.classList.remove("night");
@@ -27,6 +54,11 @@ class Application {
         document.body.classList.add("night");
         window.localStorage.setItem("theme", "night");
       }
+    });
+    this.voiceInputBtn.addEventListener("click", () => {
+      this.taskInput.placeholder = "Говорите...";
+      this.voiceInputBtn.classList.add('active');
+      this.speechRecognition.start();
     });
 
     document.body.addEventListener("click", (e) => {
@@ -64,7 +96,21 @@ class Application {
     if (!!theme) {
       document.body.classList.add(theme);
     }
-   }
+  }
+
+  handleSpeechRecognition(e) {
+    this.taskInput.placeholder = "Распознавание...";
+    this.voiceInputBtn.classList.remove('active');
+    const result = e.results[0];
+    if (result.isFinal) {
+      this.taskInput.value = result[0].transcript;
+      this.taskInput.placeholder = "Введите название задачи";
+    }
+  }
+
+  saveState() {
+    window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this.tasks));
+  }
 
   update() {
     let todoTasksHTML = this.tasks.filter(task => task.status === STATUSES.todo).map((task) => task.render()).join("");
@@ -75,62 +121,105 @@ class Application {
     this.inProgressTasksList.innerHTML = inProgressTasksHTML;
     this.doneTasksList.innerHTML = doneTasksHTML;
 
-    window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this.tasks));
+    this.saveState()
   }
 
-  addTask() {
-    let taskName = prompt("Введите название задачи");
+  updateTask() {
+    let taskName = this.taskInput.value;
     if (!taskName) {
       return;
     }
 
-    let task = new Task({ name: taskName });
-    this.tasks.push(task);
+    if (!this.editMode) {
+      let task = new Task({ name: taskName, appearanceAnimation: true });
+      this.tasks.push(task);
+    } else {
+      this.editedTask.name = taskName;
+      this.editedTask.editing = false;
+      this.editedTask = null;
+      this.editMode = false;
+    }
 
     this.update();
+    this.tasks.forEach(task => task.appearanceAnimation = false);
+    this.saveState();
+    this.taskInput.value = "";
   }
 
   destroyTask(taskId) {
     const idsArray = this.tasks.map(task => task.id);
-    const taskToDestroy = idsArray.indexOf(+taskId);
-    if (taskToDestroy === -1) {
+    const taskToDestroy = this.tasks.find(task => task.id);
+    const taskToDestroyId = idsArray.indexOf(+taskId);
+    if (taskToDestroyId === -1) {
       return;
     }
-
-    this.tasks.splice(taskToDestroy, 1);
+    taskToDestroy.disappearanceAnimation = true;
     this.update();
+
+    this.tasks.splice(taskToDestroyId, 1);
+    this.saveState();
   }
 
   editTask(taskId) {
+    this.editMode = true;
     let task = this.tasks.find(task => task.id === +taskId);
-    let taskEdit = prompt("Введите изменения", task.name);
-    if(!taskEdit) {
-      return;
-    }
-    task.name = taskEdit;
-
+    this.editedTask = task;
+    task.editing = true;
+    this.taskInput.value = task.name;
+    this.taskInput.select();
+    this.taskInput.focus();
     this.update();
   }
 
   moveInProgress(taskId) {
     let task = this.tasks.find(task => task.id === +taskId);
-    task.status = STATUSES.inProgress;
+    new Promise((resolve) => {
+      task.disappearanceAnimation = true;
+      this.update();
+      setTimeout(() => resolve(), 150);
+    }).then(() => {
+      task.status = STATUSES.inProgress;
+      task.appearanceAnimation = true;
+      task.disappearanceAnimation = false;
+      this.update();
 
-    this.update();
+      this.tasks.forEach(task => task.appearanceAnimation = false);
+      this.saveState();
+    })
   }
 
   moveInTodo(taskId) {
     let task = this.tasks.find(task => task.id === +taskId);
-    task.status = STATUSES.todo;
+    new Promise((resolve) => {
+      task.disappearanceAnimation = true;
+      this.update();
+      setTimeout(() => resolve(), 150);
+    }).then(() => {
+      task.status = STATUSES.todo;
+      task.appearanceAnimation = true;
+      task.disappearanceAnimation = false;
+      this.update();
 
-    this.update();
+      this.tasks.forEach(task => task.appearanceAnimation = false);
+      this.saveState();
+    })
   }
 
   moveInDone(taskId) {
     let task = this.tasks.find(task => task.id === +taskId);
-    task.status = STATUSES.done;
+    new Promise((resolve) => {
+      task.disappearanceAnimation = true;
+      this.update();
+      setTimeout(() => resolve(), 150);
+    }).then(() => {
+      task.status = STATUSES.done;
+      task.appearanceAnimation = true;
+      task.disappearanceAnimation = false;
+      this.update();
 
-    this.update();
+      this.tasks.forEach(task => task.appearanceAnimation = false);
+      this.saveState();
+    })
   }
 
 }
@@ -141,6 +230,9 @@ class Task {
     this.name = options.name;
     this.id = options.id || Date.now();
     this.status = options.status || STATUSES.todo;
+    this.appearanceAnimation = options.appearanceAnimation || false;
+    this.disappearanceAnimation = options.disappearanceAnimation || false;
+    this.editing = false;
   }
 
   render() {
@@ -148,7 +240,10 @@ class Task {
     let buttonsForTaskInInProgress = this.status === STATUSES.inProgress ? `<button class="to-to-do" data-id="${this.id}"><img src="./images/rewind.svg" /></button><button class="to-done" data-id="${this.id}"><img src="./images/finish.svg" /></button>` : "";
     let buttonsForTaskInDone = this.status === STATUSES.done ? `<button class="to-to-do" data-id="${this.id}"><img src="./images/rewind.svg" /></button>` : "";
     return `
-      <div class="task">
+      <div class="task
+        ${this.appearanceAnimation ? 'animation-in' : null}
+        ${this.disappearanceAnimation ? 'animation-out' : null}
+        ${this.editing ? 'editing' : null}">
         <div class="name">
           <p>${this.name}</p>
         </div>
